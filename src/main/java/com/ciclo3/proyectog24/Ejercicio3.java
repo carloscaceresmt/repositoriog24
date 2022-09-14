8524265559a20790bf68020fb24a4f3f2d992ea1
carlos caceres <carlos.caceres.mt@usa.edu.co>
2022-09-14 18:40:38 -0500

    Agregando Clase Ejercicio3.java

---
+++ src/main/java/com/ciclo3/proyectog24/Ejercicio3.java
@@ -0,0 +1,22 @@
+package com.ciclo3.proyectog24;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author DELL
+ */
+public class Ejercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola sigo programando");
+    }
+    
+}
