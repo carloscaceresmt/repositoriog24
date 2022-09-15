488e60ae5196c47076637f7e6d90900d3377340e
carlos caceres <carlos.caceres.mt@usa.edu.co>
2022-09-14 19:12:31 -0500

    agregando funcionalidad Ejercicio1.java

--- src/main/java/com/ciclo3/proyectog24/Ejercicio1.java
+++ src/main/java/com/ciclo3/proyectog24/Ejercicio1.java
@@ -12,5 +12,6 @@ public class Ejercicio1 {
     public void metodo(){
         System.out.println("Logica metodo");
         System.out.println("Agregando logica");
+        System.out.println("agregar en metodo 1");
     }
 }
